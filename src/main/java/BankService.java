0c4e75ed64e3be1503e254e51117340efe046cd4
Vengal Reddy Sandeep <vengalreddysandeep@gmail.com>
2024-06-11 09:08:38 +0530

    Done

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,7 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
-
+        this.balance=amount+this.balance;
     }
 
     /**
@@ -32,7 +32,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
-
+        if(this.balance-amount<0){
+        }
+        else {
+            this.balance=this.balance-amount;
+        }
     }
 
     /**
@@ -40,6 +44,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return this.balance;
     }
 }
